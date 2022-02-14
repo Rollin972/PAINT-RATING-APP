0b2a1f904d7f57fb64da0ea7d549e014f4e1643d
Rollin754 <75938189+Rollin972@users.noreply.github.com>
2022-02-14 15:21:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+//Used To Send The User To The Rating Area With The Buttons Foe Images : Start
+function start() {
+    window.location = "Rate.html";
+  }
+
+  function logout() {
+    window.location = "Rate.html";
+  }
+//Used To Send The User To The Rating Area With The Buttons Foe Images : End
+
+//Used For Redirecting The user To The HTML Page With The Image : Start
+function image1() {
+    window.location = "image1.html";
+  }
+
+function image2() {
+    window.location = "image2.html";
+  }
+
+function image3() {
+    window.location = "image3.html";
+  }
+
+function image4() {
+    window.location = "image4.html";
+  }
+
+function image5() {
+    window.location = "image5.html";
+  }
+
+function image6() {
+    window.location = "image6.html";
+  }
+
+function image7() {
+    window.location = "image7.html";
+  }
+//Used For Redirecting The user To The HTML Page With The Image : End
+
+
+//Used For Redirecting The user To The Start Screen : Start
+function startarea() {
+  window.location = "Start.html";
+}
+//Used For Redirecting The user To The Start Screen : End
